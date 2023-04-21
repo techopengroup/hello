c530d24ca083372e404f04efab639b73ecbed607
techopengroup <130648038+techopengroup@users.noreply.github.com>
2023-04-21 15:34:05 +0530

    Add files via upload

---
+++ Hello1/src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/helloworld")
+	 public String hello(){
+		return "Hi Hello World1";
+			
+	}
+
+}
